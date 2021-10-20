19a577662cc519b3b2344e1dc41d624915584aea
clem-ant <clementfourmii@hotmail.fr>
2021-10-20 17:47:16 +0200

    Changement de la methode modifier_utilisateur pour la faire match avec les setters et les getters

--- Administrateur.py
+++ Administrateur.py
@@ -10,6 +10,7 @@ class Administrateur:
 
     def afficher_utilisateur_list(self):
         for user in self.utilisateur:
+            print("\n")
             print("Nom : " + user.get_nom())
             print("Prenom : " + user.get_prenom())
             print("Email : " + user.get_email())
@@ -32,15 +33,39 @@ class Administrateur:
                 else:
                     print("On ne trouve pas l'utilisateur")
 
-    def modifier_utilisateur(self, nom, prenom, index_changement, valeur_a_mettre, num_port = ""):
+    def modifier_utilisateur(self, nom, prenom, num_port = ""):
+        while True:
+            print("Quel élément voulez vous changer : ")
+            index_changement = int(input("[1] Nom\n[2] Prenom\n[3] Email\n[4] Numéro de Portable\n[5] Adresse Postale\n"))
+            if(index_changement > 0 or index_changement <= 5):
+                break
+        valeur_a_mettre = input("Par quoi voulez vous le remplacer ?\n")
         if num_port != "":
             for user in range(len(self.utilisateur)):
                 if(self.utilisateur[user].get_num_port() != "" and self.utilisateur[user].get_num_port() == num_port):
-                    self.utilisateur[user][index_changement] = valeur_a_mettre
+                    if index_changement == 1:
+                        self.utilisateur[user].set_nom(valeur_a_mettre)
+                    elif index_changement == 2:
+                        self.utilisateur[user].set_prenom(valeur_a_mettre)
+                    elif index_changement == 3:
+                        self.utilisateur[user].set_email(valeur_a_mettre)
+                    elif index_changement == 4:
+                        self.utilisateur[user].set_num_port(valeur_a_mettre)
+                    else:
+                        self.utilisateur[user].set_adresse_postale(valeur_a_mettre)
         else:
             for user in range(len(self.utilisateur)):
                 if(self.utilisateur[user].get_nom() == nom and self.utilisateur[user].get_prenom() == prenom):
-                    self.utilisateur[user][index_changement] = valeur_a_mettre
+                    if index_changement == 1:
+                        self.utilisateur[user].set_nom(valeur_a_mettre)
+                    elif index_changement == 2:
+                        self.utilisateur[user].set_prenom(valeur_a_mettre)
+                    elif index_changement == 3:
+                        self.utilisateur[user].set_email(valeur_a_mettre)
+                    elif index_changement == 4:
+                        self.utilisateur[user].set_num_port(valeur_a_mettre)
+                    else:
+                        self.utilisateur[user].set_adresse_postale(valeur_a_mettre)
                 else:
                     print("On ne trouve pas l'utilisateur")
 
@@ -52,4 +77,7 @@ def afficher_list(liste):
 admin = Administrateur("PAYET", "Clément")
 admin.ajouter_utilisateur("ESTIENNE", "Clément", "niah-niah@hotmail.fr")
 admin.ajouter_utilisateur("DUGUAIT", "Nicolas", "nicolas.duguait@hotmail.fr", "0624522323", "2 rue de la gare, Toulouse, 31400")
+admin.supprimer_utilisateur("DUGUAIT", "Nicolas")
+admin.modifier_utilisateur("ESTIENNE", "Clément")
+
 admin.afficher_utilisateur_list()
--- Utilisateur.py
+++ Utilisateur.py
@@ -10,14 +10,29 @@ class Utilisateur:
     def get_nom(self):
         return self.nom
 
+    def set_nom(self, nom):
+        self.nom = nom
+        
     def get_prenom(self):
         return self.prenom
+
+    def set_prenom(self, prenom):
+        self.prenom = prenom
 
     def get_email(self):
         return self.email
 
+    def set_email(self, email):
+        self.email = email
+
     def get_num_port(self):
         return self.num_port
 
+    def set_num_port(self, num_port):
+        self.num_port = num_port
+
     def get_adresse_postale(self):
         return self.adresse_postale
+
+    def set_adresse_postale(self, adresse_postale):
+        self.adresse_postale = adresse_postale
