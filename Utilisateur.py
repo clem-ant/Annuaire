8c1c7f5c6fda5805981f8dc37e37a83b4a85c575
clem-ant <clementfourmii@hotmail.fr>
2021-10-25 16:26:31 +0200

    Ajout de l'annuaire, j'ai pas reussi a utiliser une fonction 'verifier_si_dans_les_contacts' en gros pour ajouter un contact et pas faire de doublons. + fonctions ajouter_contact et retirer_contact

--- Utilisateur.py
+++ Utilisateur.py
@@ -1,4 +1,5 @@
 class Utilisateur:
+    #TODO se connecter au serveur
 
     def __init__(self, nom, prenom, email, num_port = "", adresse_postale =""):
         self.nom = nom
@@ -6,6 +7,8 @@ class Utilisateur:
         self.email = email
         self.num_port = num_port
         self.adresse_postale = adresse_postale
+        self.contact = "contact_"+nom+"_"+prenom+".csv"
+        
 
     def get_nom(self):
         return self.nom
@@ -36,3 +39,42 @@ class Utilisateur:
 
     def set_adresse_postale(self, adresse_postale):
         self.adresse_postale = adresse_postale
+    
+    def verifier_si_unique(self, nom, prenom, email):
+        fichierContact = open(self.contact, "r")
+        contact = fichierContact.read().splitlines()
+        for cont in contact:
+            splited = cont.split(",")
+            if(splited[0] == nom and splited[1] == prenom and splited[2] == email):
+                return False
+        return True
+    
+    def ajouter_contact(self, nom, prenom, email, num_port = "", adresse_postale = ""):
+        fichierContact = open(self.contact, "r")
+        contact = fichierContact.read().splitlines()
+        for cont in contact:
+            splited = cont.split(",")
+            if(splited[0] == nom and splited[1] == prenom and splited[2] == email):
+                print("L'utilisateur est déjà dans votre Annuaire")
+                return
+        fichierContact = open(self.contact, "a")
+        fichierContact.write(nom+","+prenom +","+ email +","+ num_port + "," + adresse_postale + "\n")
+        fichierContact.close()
+
+    def retirer_contact(self, nom, prenom, num_port= ""):
+        fichierContact = open(self.contact, "r")
+        infos = fichierContact.read().splitlines()
+        new_file = open(self.contact, "w")
+        if(len(infos) == 0):
+            print("Il n'y a personne dans votre Annuaire, vous ne pouvez donc pas en supprimer")
+            return
+        if num_port != "":
+            for info in infos:
+                splited = info.split(",")
+                if splited[0] != nom and splited[1] != prenom:
+                    new_file.write(info + "\n")
+        else:
+            for info in infos:
+                splited = info.split(",")
+                if splited[0] != nom and splited[1] != prenom:
+                    new_file.write(info + "\n")
